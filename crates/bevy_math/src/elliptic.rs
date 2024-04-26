4eb27d82c9c3145bd6009bffb0cc7a976b274381
Raphael Büttgenbach <62256001+solis-lumine-vorago@users.noreply.github.com>
2024-04-26 15:55:04 +0200

    Add `cel`

--- crates/bevy_math/src/elliptic.rs
+++ crates/bevy_math/src/elliptic.rs
@@ -1,7 +1,235 @@
 //! This module contains methods for computing elliptic integrals
 #![deny(clippy::excessive_precision)]
-use std::f32::consts::PI;
+use std::f32::{consts::PI, INFINITY, NAN};
 
+/// Computes the incomplete elliptic integral of the second kind for `phi` and `m`: E(phi | m).
+/// The result will be accurate within ten machine epsilons
+///
+/// `phi` must be in the range `(0, PI / 2)` and
+/// `m` must be in the range `(0, 1)`
+pub fn el2(x: f32, m: f32) -> f32 {
+    todo!();
+}
+
+/// Computes the incomplete elliptic integral of the second kind for `m`: E(m).
+/// The result will be accurate within ten machine epsilons
+///
+/// `m` must be in the range `(0, 1)`
+pub fn cel2(m: f32) -> f32 {
+    if m == 0. {
+        return PI / 2.;
+    }
+    if m == 1. {
+        return 1.;
+    }
+
+    if m < -2e6 {
+        let b = (-16. * m).ln();
+        let a = (-m).sqrt();
+        return a + 0.25 * (b + 1.) / a - 1. / 32. * (b - 1.5) / a.powi(3);
+    };
+
+    if m > 0.99999 {
+        let m1 = 1. - m;
+        let a = (16. / m1).ln();
+        return 1. + m1 * 0.25 * (a - 1.);
+    };
+
+    2. * rg(0., 1. - m, 1.)
+}
+
+/// Computes the Carlson degenerate integral of [`el1`]
+fn rc(x: f32, y: f32) -> f32 {
+    if y == 0. {
+        return f32::INFINITY;
+    }
+    if x == 0. && y > 0. {
+        return PI / (2. * y.sqrt());
+    }
+    if x == y {
+        return 1. / x.sqrt();
+    }
+
+    let (w, mut x, mut y) = if y > 0. {
+        (1., x, y)
+    } else {
+        ((x / (x - y)).sqrt(), x - y, -y)
+    };
+
+    const ERRTOL: f32 = 3e-4;
+
+    let (mut m, mut s);
+    loop {
+        let p = 2. * (x * y).sqrt() + y;
+        x = 0.25 * (x + p);
+        y = 0.25 * (y + p);
+        s = x + y + y;
+        m = s / 3.;
+        s = (y - x) / s;
+        if s.is_nan() {
+            return f32::NAN;
+        }
+        if s.abs() < ERRTOL {
+            break;
+        }
+    }
+
+    const C1: f32 = 0.3;
+    const C2: f32 = 1. / 7.;
+    const C3: f32 = 0.375;
+    const C4: f32 = 9. / 22.;
+    let s = (C1 + s * (C2 + s * (C3 + s * C4))) * s * s;
+
+    w * ((1. + s) / m.sqrt())
+}
+
+/// Computes the Carlson degenerate cases of [`cel2`]
+fn rd(mut x: f32, mut y: f32, mut z: f32) -> f32 {
+    if x + y == 0. {
+        return f32::NAN;
+    }
+    if x == y && x == 0. {
+        return f32::INFINITY;
+    }
+
+    if y == z {
+        if x == 0. {
+            return 3. * PI / (4. * y * y.sqrt());
+        }
+        if x == y {
+            return 1. / (x * x.sqrt());
+        }
+    }
+
+    const ERRTOL: f32 = 0.002;
+
+    let mut sm = 0.;
+    let mut fc = 1.;
+    let (mut dx, mut dy, mut dz, mut m);
+    loop {
+        let rx = x.sqrt();
+        let ry = y.sqrt();
+        let rz = z.sqrt();
+        let lm = rx * (ry + rz) + ry * rz;
+        sm = sm + fc / (rz * (z + lm));
+        fc = 0.25 * fc;
+        x = 0.25 * (lm + x);
+        y = 0.25 * (lm + y);
+        z = 0.25 * (lm + z);
+        m = (x + y + 3. * z) / 5.;
+        if m.is_infinite() {
+            return f32::NAN;
+        }
+        dx = (m - x) / m;
+        dy = (m - y) / m;
+        dz = (m - z) / m;
+        if dx.abs() < ERRTOL && dy.abs() < ERRTOL && dz.abs() < ERRTOL {
+            break;
+        }
+    }
+    let ea = dx * dy;
+    let eb = dz * dz;
+    let ec = ea - eb;
+    let ed = ea - 6. * eb;
+    let ee = ed + ec + ec;
+
+    const C1: f32 = -3. / 14.;
+    const C2: f32 = 1. / 6.;
+    const C3: f32 = 9. / 22.;
+    const C4: f32 = 3. / 26.;
+    const C5: f32 = 9. / 88.;
+    const C6: f32 = 9. / 52.;
+    let s = ed * (C1 + C5 * ed - C6 * dz * ee);
+    let s = s + dz * (C2 * ee + dz * (dz * C4 * ea - C3 * ec));
+
+    3. * sm + fc * (1. + s) / (m * m.sqrt())
+}
+
+/// Computes the Carlson elliptic integral of the first kind
+fn rf(mut x: f32, mut y: f32, mut z: f32) -> f32 {
+    if z == 0. && x > 0. {
+        std::mem::swap(&mut x, &mut z);
+    }
+    if y == 0. && x > 0. {
+        std::mem::swap(&mut x, &mut y);
+    }
+
+    if x == 0. && y == 0. {
+        return INFINITY;
+    }
+    if x == y && y == z {
+        return 1. / x.sqrt();
+    }
+
+    if y == z {
+        return if x == 0. {
+            PI / (2. * y.sqrt())
+        } else {
+            rc(x, y)
+        };
+    }
+
+    let (mut dx, mut dy, mut dz, mut av);
+    const ERRTOL: f32 = 0.001;
+    loop {
+        let rx = x.sqrt();
+        let ry = y.sqrt();
+        let rz = z.sqrt();
+        let lm = rx * (ry + rz) + ry * rz;
+        x = 0.25 * (lm + x);
+        y = 0.25 * (lm + y);
+        z = 0.25 * (lm + z);
+        av = (x + y + z) / 3.;
+        if av.is_infinite() {
+            return NAN;
+        }
+        dx = (av - x) / av;
+        dy = (av - y) / av;
+        dz = (av - z) / av;
+        if dx.abs() < ERRTOL && dy.abs() < ERRTOL && dz.abs() < ERRTOL {
+            break;
+        }
+    }
+    let e2 = dx * dy - dz * dz;
+    let e3 = dx * dy * dz;
+
+    const C1: f32 = 1. / 24.;
+    const C2: f32 = 1. / 10.;
+    const C3: f32 = 3. / 44.;
+    const C4: f32 = 1. / 14.;
+    let s = e2 * (C1 * e2 - C2 - C3 * e3) + C4 * e3;
+
+    (1. + s) / av.sqrt()
+}
+
+/// Computes the Carlson's completely symmetric elliptic integral of the 2nd kind
+fn rg(x: f32, y: f32, z: f32) -> f32 {
+    let [y, z, x] = {
+        let mut xyz = [x, y, z];
+        xyz.sort_by(f32::total_cmp);
+        xyz
+    };
+    if x == y {
+        return x.sqrt();
+    }
+    if z == 0. {
+        return 0.5 * x.sqrt();
+    }
+    if y == 0. && x == z {
+        return PI / 4. * x.sqrt();
+    }
+
+    let s = ((x / z) * y).sqrt();
+    let f = z * rf(x, y, z);
+    let d = if z == x || z == y {
+        0.
+    } else {
+        let d = rd(x, y, z) / 3.;
+        let d = (x - z) * d;
+        (z - y) * d
+    };
+    0.5 * (f + d + s)
+}
 /// Computes the incomplete elliptic integral of the first kind for `phi` and `m`: F(phi | m).
 /// The result will be accurate within ten machine epsilons
 ///
@@ -123,16 +351,16 @@ fn serf(y: f32, m: f32) -> f32 {
 /// `m` should be in the range `(0, 1)`
 pub fn cel1(m: f32) -> f32 {
     if m < 0.9 {
-        elliptic_k_small(m)
+        cel1_small(m)
     } else {
-        elliptic_k_large(m)
+        cel1_large(m)
     }
 }
 
 /// Computes the complete elliptic integral of the first kind for a given `m`:
 ///
 /// `m` should be in the range `(0, 0.9)`
-fn elliptic_k_small(m: f32) -> f32 {
+fn cel1_small(m: f32) -> f32 {
     let index = if m < 0.8 {
         (m / 0.1) as usize
     } else {
@@ -215,9 +443,9 @@ fn elliptic_k_small(m: f32) -> f32 {
 /// Computes the complete elliptic integral of the first kind for a given `m`:
 ///
 /// `m` should be in the range `(0.9, 1)`
-fn elliptic_k_large(m: f32) -> f32 {
+fn cel1_large(m: f32) -> f32 {
     let m_c = 1. - m;
-    let k_c = elliptic_k_small(m_c);
+    let k_c = cel1_small(m_c);
 
     // These values are the coefficients of the Maclaurin expansion of q_c with respect to m_c
     const COEFFICIENTS: [f32; 6] = [
@@ -239,7 +467,7 @@ fn elliptic_k_large(m: f32) -> f32 {
 }
 
 mod tests {
-    use crate::elliptic::{cel1, el1};
+    use crate::elliptic::{cel1, cel2, el1};
     use approx::assert_relative_eq;
     use std::{
         f32::consts::{FRAC_PI_2, FRAC_PI_4},
@@ -288,5 +516,51 @@ mod tests {
         assert_relative_eq!(el1(FRAC_PI_4, 0.5), 0.8260179, max_relative = FIVE_EPSILON);
         assert_relative_eq!(el1(FRAC_PI_4, 0.75), 0.8512237, max_relative = FIVE_EPSILON);
         assert_relative_eq!(el1(FRAC_PI_4, 1.0), 0.8813736, max_relative = FIVE_EPSILON);
+    }
+
+    #[test]
+    fn complete_integral_second_kind() {
+        assert_relative_eq!(cel2(0.062), 1.5461584);
+        assert_relative_eq!(cel2(0.158), 1.5067791);
+        assert_relative_eq!(cel2(0.217), 1.4817567);
+        assert_relative_eq!(cel2(0.334), 1.4300116);
+        assert_relative_eq!(cel2(0.409), 1.3951269);
+        assert_relative_eq!(cel2(0.582), 1.3081238);
+        assert_relative_eq!(cel2(0.618), 1.2885870);
+        assert_relative_eq!(cel2(0.753), 1.2091616);
+        assert_relative_eq!(cel2(0.829), 1.1584934);
+        assert_relative_eq!(cel2(0.964), 1.0463593);
+        assert_relative_eq!(cel2(0.99992), 1.0002241);
+        assert_relative_eq!(cel2(0.999995), 1.0000175);
+    }
+
+    #[test]
+    fn stress() {
+        const N: usize = 1_000_000;
+        let mut random = Vec::with_capacity(N);
+
+        for _ in 0..N {
+            random.push(rand::random());
+        }
+
+        let mut cloned = random.clone();
+        let start = Instant::now();
+        for r in random.iter_mut() {
+            *r = cel2(*r);
+        }
+        let int_dur = Instant::now().duration_since(start);
+
+        let start = Instant::now();
+        for r in cloned.iter_mut() {
+            *r = (*r).sin();
+        }
+        let sin_dur = Instant::now().duration_since(start);
+
+        println!("{N} integrals took {}ms", int_dur.as_millis());
+        println!("{N} sin() took {}ms", sin_dur.as_millis());
+        println!(
+            "Integrals are {:.2} times slower",
+            int_dur.as_secs_f64() / sin_dur.as_secs_f64()
+        );
     }
 }
